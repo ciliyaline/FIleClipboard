b60cf2bb81a7feb52a834d03da12ec13cc011993
sw <1136469680@qq.com>
2024-02-20 21:23:18 +0800

    一段...跑不起来的代码

--- test_1.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Header, Cookie
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/items/")
-def read_item(user_agent: str = Header(None), session_token: str = Cookie(None)):
-    return {"User-Agent": user_agent, "Session-Token": session_token}
